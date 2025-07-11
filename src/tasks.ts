c66e8e84135e612ac0a2ee1b6f42d6acb5c92e8e
Seb <quillsdrgntmr@gmail.com>
2025-07-11 19:28:22 +1000

    tasks file

---
+++ src/tasks.ts
@@ -0,0 +1,25 @@
+
+//TODO: a user needs to be able to create a task
+// A task needs a due date 
+// A description
+// when it was created 
+// when it was finished 
+// unique identifier
+
+function createTask() {
+
+}
+
+
+function editTask() {
+
+}
+
+
+function completeTask() {
+
+}
+
+function deleteTask() {
+
+}
