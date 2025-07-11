7debbe713671e9bb1ecd507ced3281765d89d6e3
Franz Joseph Goseco <z5591880@ad.unsw.edu.au>
2025-07-11 20:36:24 +1000

    gtgtgtggt

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,21 +1,20 @@
+import { dataStoreObj } from './interfaces';
+
 const fs = require('fs');
 
-
-let data = {};
+let data: dataStoreObj = {
+  classrooms: [],
+};
 
 export function load() {
-    const json = fs.readFileSync('data.json', { flag: 'r' });
-    data = JSON.parse(json.toString());
+  const json = fs.readFileSync('data.json', { flag: 'r' });
+  data = JSON.parse(json.toString());
 }
-
 
 export function setData(newData) {
-    data = newData;
-    const json = JSON.stringify(newData, null, 4);
-    fs.writeFileSync('data.json', json, { flag: 'w' });
+  data = newData;
+  const json = JSON.stringify(newData, null, 4);
+  fs.writeFileSync('data.json', json, { flag: 'w' });
 }
 
-
-
-
-
+export { data };
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,37 +1,38 @@
 export interface UserDetails {
-    user: {
-        userId: string,
-        username: string, 
-        name: string,
-        email: string,
-        class: string,
-    }
+  user: {
+    userId: string;
+    username: string;
+    name: string;
+    email: string;
+    class: string;
+  };
 }
 
 export interface User {
-    userId: string,
-    username: {},
-    name: string,
-    email: string,
-    class: string,
-    password: string,
-    sessions: string[],
-    classroom: Classroom,
+  userId: string;
+  username: {};
+  name: string;
+  email: string;
+  class: string;
+  password: string;
+  sessions: string[];
+  classroom: Classroom;
 }
 
 export interface Classroom {
-    name: string,
-    students: User[],
-    teacher: User,
+  id: number;
+  name: string;
+  students: User[];
+  teacher: User[];
 }
 
 export interface Task {
-    timeCreated: number,
-    dueDate: number,
-    details: string,
-    completed: boolean,
+  timeCreated: number;
+  dueDate: number;
+  details: string;
+  completed: boolean;
 }
 
 export interface dataStoreObj {
-    classrooms: Classroom[],
+  classrooms: Classroom[];
 }
