6639dc6af36e80aa4af5b7f1d65c0406fe3d0122
Seb <quillsdrgntmr@gmail.com>
2025-07-11 20:29:11 +1000

    qwerqw

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,8 +1,11 @@
 
 
-interface task {
-
-};
+export interface Task {
+    timeCreated: number,
+    dueDate: number,
+    details: string,
+    completed: boolean,
+}
 
 
 //TODO: a user needs to be able to create a task
@@ -12,7 +15,19 @@ interface task {
 // when it was finished 
 // unique identifier
 
-function createTask(description: string,): task {
+function createTask(userId: string, description: string, due_date: number): Task {
+    //TODO: check valid date
+
+    const task: Task = {
+        timeCreated: Math.floor(Date.now() / 1000),
+        dueDate: due_date,
+        details: description,
+        completed: false,
+    };
+
+
+
+
 
 }
 
