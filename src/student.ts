eb8ebb37ed9e14941f281a90c0e18dbf9b3b6e1a
Seb <quillsdrgntmr@gmail.com>
2025-07-11 23:56:56 +1000

    feat: add, edit, remove students

--- src/student.ts
+++ src/student.ts
@@ -40,7 +40,79 @@ any id's in classroom: ${classroom.name}`);
 //password: string,
 //sessions: string[],
 //classroom: Classroom,
+function updateStudent(studentid: number, classroomId: number,
+    changes: {
+        name?: string,
+        email?: string,
+        password?: string,
+        classroom?: Classroom
+    }): boolean {
 
-function updateStudent(studentid: number,): boolean {
+    const student = getStudent(studentid, classroomId);
 
+    if (student === undefined) {
+        return false;
+    }
+
+    if (changes.name) {
+        student.name = changes.name;
+    }
+
+    if (changes.email) {
+        student.email = changes.email;
+    }
+
+    if (changes.password) {
+        student.password = changes.password;
+    }
+
+    if (changes.classroom) {
+        student.classroom = changes.classroom;
+    }
+
+    return true;
 }
+
+function changePassword(studentid: number, classroomId: number,
+    oldPassword: string, newPassword: string): boolean {
+    const student = getStudent(studentid, classroomId);
+
+    if (student === undefined) {
+        return false;
+    }
+
+    if (student.password === oldPassword) {
+        student.password = newPassword;
+        return true;
+    }
+    return false;
+}
+
+
+function removeStudent(studentid: number, classroomId: number) {
+    const classroom = data.classrooms
+        .find((classroom: Classroom) => {
+            return classroom.classroomId === classroomId
+        });
+
+    if (classroom === undefined) {
+        console.log(`The classroom id you provided ${classroomId} does not match 
+any classrooms`);
+        return false;
+    }
+
+    const student = classroom.students.find((user: User) => {
+        return user.userId == studentid;
+    });
+
+    if (student === undefined) {
+        console.log(`The student id you provided ${studentid} does not match 
+any id's in classroom: ${classroom.name}`);
+        return false;
+    }
+
+    data.userIds.splice(data.userIds.indexOf(student.userId), 1);
+    classroom.students.splice(classroom.students.indexOf(student), 1);
+    return true;
+}
+
