57ca580fbc2c2045acc2837cb93380f9bd4a040e
Seb <quillsdrgntmr@gmail.com>
2025-07-11 20:43:54 +1000

    qwerqw

---
+++ src/student.ts
@@ -0,0 +1,10 @@
+import { User } from "./interfaces";
+import { data } from "./dataStore";
+
+
+//get a student
+//update a student 
+//remove a studnet
+function getStudent(studentid: number, classroomId: number): User {
+
+}
